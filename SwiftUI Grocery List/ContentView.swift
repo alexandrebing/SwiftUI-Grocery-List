9471f5b0fa12cca8e374c38617a21a15c440b737
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-04-22 22:23:06 -0300

    First commit

---
+++ SwiftUI Grocery List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Grocery List
+//
+//  Created by Alexandre Scheer Bing on 22/04/20.
+//  Copyright © 2020 Alexandre Bing. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
